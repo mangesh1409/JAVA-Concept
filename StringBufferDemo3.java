3f86966168eda2c639966638f94d9f0dcafd9b5e
Mangesh Prakash Jadhav <42151779+mangesh1409@users.noreply.github.com>
2020-02-01 01:06:02 +0530

    Add files via upload

---
+++ StringBufferDemo3.java
@@ -0,0 +1,27 @@
+class String1 extends String
+{
+	String1()
+	{
+		System.out.println("Can be extended....");
+	}
+
+
+}
+class StringBuffer1 extends StringBuffer
+{
+
+	StringBuffer1()
+	{
+		System.out.println("Can be extended....");
+	}
+
+}
+
+class StringBufferDemo3
+{
+	public static void main(String args[])
+	{
+		String1 s1 = new String1();
+		StringBuffer1 sb1 = new StringBuffer1();
+	}
+}
