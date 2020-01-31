6aff178ff51d706ef92718d6c1731fc5195ee692
Mangesh Prakash Jadhav <42151779+mangesh1409@users.noreply.github.com>
2020-01-31 13:39:14 +0530

    Update FirstApplication.java

--- FirstApplication.java
+++ FirstApplication.java
@@ -1,19 +1,3 @@
-/********************************************************
-*
-*	This program demonstrate the How to write a class in java
-*	File name should be saved as a class name which contains 
-*	the main function
-*	
-*	public static void main(String args[])
-*
-*	public : the function main is public;
-*	static : the function main is called without any object
-*			i.e obj.main()
-*	void	  : 	this function doesn't return any value
-*	main() : is the name of the function	
-*	 
-********************************************************/
-
 import java.lang.*;
 
 // Class which contains the main function
